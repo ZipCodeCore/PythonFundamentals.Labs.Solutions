f3e3fdf69e60c2190272624d0bc405b9d51beed6
rpdedeus <roberto.p.dedeus@gmail.com>
2020-03-24 16:28:30 -0400

    Fix make_requests.py.

--- DataAcqusitionLab/make_request.py
+++ DataAcqusitionLab/make_request.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 
-def call_nooa(offset):
+def call_nooa(offset, token) -> bytes:
     params = urllib.parse.urlencode(
         {
             'limit': 1000,
@@ -21,13 +21,13 @@ def call_nooa(offset):
     url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?%s" % params
 
     req = urllib.request.Request(url)
-    req.add_header('Token', sys.argv[1])
+    req.add_header('Token', token)
 
     res = urllib.request.urlopen(req)
-    return res
+    return res.read()
 
 
-def write_result(data, file_name):
+def write_result(data: bytes, file_name: str):
     file_path = os.path.join("./", "data", file_name)
     with open(file_path, "wb") as d:
         d.write(data)
@@ -42,6 +42,6 @@ if __name__ == '__main__':
     file_names = ["locations_{0}.json".format(str(x - 1)[:1] if len(str(x)) < 5 else str(x)[:2]) for x in offsets]
 
     for file_name, offset in zip(file_names, offsets):
-        data = call_nooa(offset)
+        data = call_nooa(offset, sys.argv[1])
         write_result(data, file_name)
         wait(5)
